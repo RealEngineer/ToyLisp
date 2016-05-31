783fd1f55c86c37dabcfdd2c2c0e77a3b91a23f9
Vinay Emani <vinayemani@gmail.com>
2016-05-31 09:43:58 +0530

    getTypeWithId(TypeId)

--- inc/TypeObject.h
+++ inc/TypeObject.h
@@ -9,8 +9,16 @@
 #include "ToyLispObject.h"
 
 /**
- * TypeObject represents a single type in the program.
+ * TypeObject represents a single type in the program. As the program modules
+ * are interpreted, several TypeObject objects are created and registered with
+ * the TypeSystem module.
  */
 class TypeObject: public ToyLispObject
 {
 };
+
+/*
+ * Return the type(its object) with the type id or nullptr if no such type
+ * exists.
+ */
+TypeObject *getTypeWithId(const TypeId typeId);
